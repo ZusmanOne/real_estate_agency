7f1d3fce6621e74e32984dfcfbad7a336ba6466a
Nikita <bigmama93@mail.ru>
2022-05-26 14:15:05 +0300

    add db model for likes.

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat,Complaint
 
 
 class FlatAdmin(admin.ModelAdmin):
@@ -8,6 +8,12 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ('new_building',)
     search_fields = ('town', 'town_district','address')
     readonly_fields = ('created_at',)
+    list_filter = ('new_building',)
+
+
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user', 'flat_complaint')
 
 
 admin.site.register(Flat, FlatAdmin,)
+admin.site.register(Complaint, ComplaintAdmin)
